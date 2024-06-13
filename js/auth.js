29b0332f73bac3667b0bf23a62ae402a71f7c0fe
titulacioneq7 <titulacion.eq7@gmail.com>
2024-06-12 23:50:59 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -3,12 +3,12 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from 'https://w
 
 // Configuración de Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAhv8sig2xSMxE12IWirUp3ppNGMYjL6h0",
+  authDomain: "sistemareservaciones-30b6e.firebaseapp.com",
+  projectId: "sistemareservaciones-30b6e",
+  storageBucket: "sistemareservaciones-30b6e.appspot.com",
+  messagingSenderId: "392476332415",
+  appId: "1:392476332415:web:e19141f3698b81ef538a4a"
 };
 
 // Inicializar Firebase
