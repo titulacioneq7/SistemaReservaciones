3290587c0252a25cf0f2c9efa10f1ef622d90c4d
titulacioneq7 <titulacion.eq7@gmail.com>
2024-06-13 21:39:04 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhv8sig2xSMxE12IWirUp3ppNGMYjL6h0",
+  authDomain: "sistemareservaciones-30b6e.firebaseapp.com",
+  projectId: "sistemareservaciones-30b6e",
+  storageBucket: "sistemareservaciones-30b6e.appspot.com",
+  messagingSenderId: "392476332415",
+  appId: "1:392476332415:web:e19141f3698b81ef538a4a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
